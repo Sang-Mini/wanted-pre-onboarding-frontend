9beac277f5ab0284a8281ae24bb05012aa5cb221
Sang-Mini <sangmin3062@gmail.com>
2022-12-13 22:45:14 +0900

    feat : 비밀번호 길이가 8자 이상인지 확인하는 함수 구현

--- src/Routes/Login.js
+++ src/Routes/Login.js
@@ -8,7 +8,11 @@ function Login() {
     const [disabled, setDisabled] = useState(true);
     const [opacity, setOpacity] = useState(0.5);
 
+    // 이메일에 @ 포함되어있는지?
     const isValidEmail = userEmail.includes('@');
+
+    // 비밀번호 길이가 8자 이상인지?
+    const isValidPassword = userPassword.length >= 8;
 
     const handleInput = event => {
         return event.target.value;
@@ -20,6 +24,12 @@ function Login() {
         }
     }
 
+    const pwErrorHandler = () => {
+        if (!isValidPassword) {
+            alert('비밀번호 형식이 올바르지 않습니다. 8자 이상 입력해주세요.');
+        }
+    }
+
     return (
         <>
             <div>로그인</div>
@@ -27,14 +37,18 @@ function Login() {
                 <div>
                     <input type="email" className="login-input-email" placeholder="이메일을 입력해주세요" value={userEmail} onChange={(event) => {
                         setUserEmail(handleInput(event));
-
                     }}></input>
                     <button onClick={() => {
                         emailErrorHandler();
                     }}>인증</button>
                 </div>
                 <div>
-                    <input type="password" className="login-input-pw" placeholder="비밀번호를 입력해주세요"></input>
+                    <input type="password" className="login-input-pw" placeholder="비밀번호를 입력해주세요" value={userPassword} onChange={(event) => {
+                        setUserPassword(handleInput(event));
+                    }}></input>
+                    <button onClick={() => {
+                        pwErrorHandler();
+                    }}>인증</button>
                 </div>
                 <div>
                     <button type="submit" className="login-button">제출</button>
