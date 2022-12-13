f178989f7e3cf5fba58f8a9433aa48d73b87138c
Sang-Mini <sangmin3062@gmail.com>
2022-12-13 22:40:22 +0900

    feat : 이메일에 @ 포함되어있는지 확인하는 함수 구현

--- src/Routes/Login.js
+++ src/Routes/Login.js
@@ -1,10 +1,37 @@
+import { useState } from "react";
+
 function Login() {
+
+    const [userEmail, setUserEmail] = useState('');
+    const [userPassword, setUserPassword] = useState('');
+
+    const [disabled, setDisabled] = useState(true);
+    const [opacity, setOpacity] = useState(0.5);
+
+    const isValidEmail = userEmail.includes('@');
+
+    const handleInput = event => {
+        return event.target.value;
+    }
+
+    const emailErrorHandler = () => {
+        if (!isValidEmail) {
+            alert('이메일 형식이 올바르지 않습니다.');
+        }
+    }
+
     return (
         <>
             <div>로그인</div>
             <div className="login-bg">
                 <div>
-                    <input type="email" className="login-input-email" placeholder="이메일을 입력해주세요"></input>
+                    <input type="email" className="login-input-email" placeholder="이메일을 입력해주세요" value={userEmail} onChange={(event) => {
+                        setUserEmail(handleInput(event));
+
+                    }}></input>
+                    <button onClick={() => {
+                        emailErrorHandler();
+                    }}>인증</button>
                 </div>
                 <div>
                     <input type="password" className="login-input-pw" placeholder="비밀번호를 입력해주세요"></input>
